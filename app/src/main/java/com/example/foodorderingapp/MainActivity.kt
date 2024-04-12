fcb185528b659750d12a5b541850f621e0eb3202
phuonghoa2101 <phuonghoa21604@gmail.com>
2024-04-12 21:52:21 +0700

    init project

--- app/src/main/java/com/example/foodorderingapp/MainActivity.kt
+++ app/src/main/java/com/example/foodorderingapp/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.foodorderingapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.foodorderingapp.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(NavController)
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetDialog = Notification_Bottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager, "Test")
+        }
     }
 }
